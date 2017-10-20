8c521edac0a19c279e78c0a61a4061253dc2ae2e
Jozef Randjak <randjakjozef@gmail.com>
2017-10-20 21:30:34 +0200

    Add Parent Employee class

---
+++ Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace XA01
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int DailyWage { get; set; }
+
+        public Employee(string name, int dailyWage)
+        {
+            Name = name;
+            DailyWage = dailyWage;
+        }
+    }
+}
